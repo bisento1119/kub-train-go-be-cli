8db50c5f06b85901f27d419823b57d06b41352a6
Frank Dietrich <frank.dietrich@pta.de>
2023-07-24 07:49:43 +0200

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,26 @@
+# syntax=docker/dockerfile:1
+
+#FROM alpine:3.18
+FROM ubuntu:latest
+
+
+WORKDIR /.
+
+# Download Go modules
+COPY ./go-be-cli ./
+
+# Set destination for COPY
+
+
+# Copy the source code. Note the slash at the end, as explained in
+# https://docs.docker.com/engine/reference/builder/#copy
+
+# Optional:
+# To bind to a TCP port, runtime parameters must be supplied to the docker command.
+# But we can document in the Dockerfile what ports
+# the application is going to listen on by default.
+# https://docs.docker.com/engine/reference/builder/#expose
+EXPOSE 4882
+
+# Run
+CMD ["./go-be-cli"]
---
+++ main.go
@@ -0,0 +1,143 @@
+// main.go
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+type Person struct {
+	Id           int    `json:"id"`
+	Forename     string `json:"forename"`
+	Lastname     string `json:"lastname"`
+	ProfessionId int    `json:"professionId"`
+}
+
+type Profession struct {
+	Id   int    `json:"id"`
+	Name string `json:"name"`
+	Desc string `json:"desc"`
+}
+
+type Professionals struct {
+	Id         int        `json:"id"`
+	Forename   string     `json:"forename"`
+	Lastname   string     `json:"lastname"`
+	Profession Profession `json:"profession,omitempty"`
+}
+
+type Config struct {
+	GoBeAUrl  string
+	GoBeAPort int
+	GoBeAPath string
+	GoBeBUrl  string
+	GoBeBPort int
+	GoBeBPath string
+}
+
+var myConfig Config
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the HomePage!")
+	fmt.Println("Endpoint Hit: homePage")
+}
+
+func writeHttpStatus(w http.ResponseWriter, err error) {
+	w.WriteHeader(http.StatusInternalServerError)
+	w.Write([]byte("500 - Internal Server Error"))
+}
+
+func returnAllProfessionals(w http.ResponseWriter, r *http.Request) {
+	persons, err := getAllPersons()
+	if err != nil {
+		writeHttpStatus(w, err)
+		return
+	}
+
+	professions, err2 := getAllProfessions()
+	if err2 != nil {
+		writeHttpStatus(w, err)
+		return
+	}
+
+	professionsMap := make(map[int]Profession)
+	for _, prof := range professions {
+		professionsMap[prof.Id] = prof
+	}
+
+	retList := make([]Professionals, len(persons))
+
+	for i, p := range persons {
+		retList[i].Id = p.Id
+		retList[i].Lastname = p.Lastname
+		retList[i].Forename = p.Forename
+		retList[i].Profession = professionsMap[p.ProfessionId]
+	}
+	fmt.Println("Endpoint Hit: returnAllProfessionals")
+	json.NewEncoder(w).Encode(retList)
+}
+
+func getAllPersons() ([]Person, error) {
+	var url = fmt.Sprintf("http://%v:%d/%v", myConfig.GoBeAUrl, myConfig.GoBeAPort, myConfig.GoBeAPath)
+	response, err := http.Get(url)
+	if err != nil {
+		fmt.Print(err.Error())
+		return nil, err
+	}
+
+	responseData, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+
+	var responseObject []Person
+	json.Unmarshal(responseData, &responseObject)
+
+	return responseObject, nil
+}
+
+func getAllProfessions() ([]Profession, error) {
+	var url = fmt.Sprintf("http://%v:%d/%v", myConfig.GoBeBUrl, myConfig.GoBeBPort, myConfig.GoBeBPath)
+	response, err := http.Get(url)
+	if err != nil {
+		fmt.Print(err.Error())
+		return nil, err
+	}
+
+	responseData, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+
+	var responseObject []Profession
+	json.Unmarshal(responseData, &responseObject)
+
+	return responseObject, nil
+}
+
+func handleRequests() {
+	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.HandleFunc("/", homePage)
+	myRouter.HandleFunc("/professionals", returnAllProfessionals)
+	log.Fatal(http.ListenAndServe(":4882", myRouter))
+}
+
+func main() {
+	myConfig = Config{
+		GoBeAUrl:  "localhost",
+		GoBeAPort: 4880,
+		GoBeAPath: "persons",
+		GoBeBUrl:  "localhost",
+		GoBeBPort: 4881,
+		GoBeBPath: "professions",
+	}
+
+	handleRequests()
+}
